dc719ac867a241dbeb77c6ad3272d90cb17d4101
Huu Hoang <hoangduchuuvn@gmail.com>
2019-02-16 18:29:17 +0700

    [COF] MIGRATE TO ANDROID X

--- app/src/main/java/net/hdhuu/splee/home/MainActivity.kt
+++ app/src/main/java/net/hdhuu/splee/home/MainActivity.kt
@@ -1,7 +1,7 @@
 package net.hdhuu.splee.home
 
 import android.annotation.SuppressLint
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
