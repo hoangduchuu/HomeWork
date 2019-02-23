81f04bfe5c6c2a4e5c7e388660a2bf85299e2477
Huu Hoang <hoangduchuuvn@gmail.com>
2019-02-23 14:21:30 +0700

    [DEV] add login screen

---
+++ app/src/main/java/net/hdhuu/splee/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package net.hdhuu.splee.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import net.hdhuu.splee.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
